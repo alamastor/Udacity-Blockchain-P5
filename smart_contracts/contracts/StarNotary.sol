4f238c9a812e8e2ee54e2507d44dd9a2d92c64bf
Alistair McKelvie <alistair.mckelvie@gmail.com>
2018-10-29 11:09:28 +1100

    fix linting errors

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract StarNotary is ERC721 { 
 
@@ -20,17 +20,17 @@ contract StarNotary is ERC721 {
     }
 
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
-        require(this.ownerOf(_tokenId) == msg.sender);
+        require(this.ownerOf(_tokenId) == msg.sender, "Seller must be star owner.");
 
         starsForSale[_tokenId] = _price;
     }
 
     function buyStar(uint256 _tokenId) public payable { 
-        require(starsForSale[_tokenId] > 0);
+        require(starsForSale[_tokenId] > 0, "Star must be for sale.");
         
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
-        require(msg.value >= starCost);
+        require(msg.value >= starCost, "Value must be equal or greater than star cost.");
 
         _removeTokenFrom(starOwner, _tokenId);
         _addTokenTo(msg.sender, _tokenId);
